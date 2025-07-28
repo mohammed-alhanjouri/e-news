bd47e809ff804cf7517fb21c31a2e329afa6e94b
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-07-28 16:49:41 +0300

    Database Configuration

---
+++ config/db.php
@@ -0,0 +1,17 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'Moha');
+define('DB_PASSWORD', '01001101');
+define('DB_NAME', 'news_db');
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+// else {
+//     echo "Connected successfully";
+// }
+
+
+?>
