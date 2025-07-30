a3df6efb589ed4773438c2719067f93e365e2f03
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-07-30 23:11:37 +0300

    Create assets File for CSS File and Images

--- config/db.php
+++ config/db.php
@@ -6,6 +6,7 @@ define('DB_PASSWORD', '01001101');
 define('DB_NAME', 'news_db');
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 } 
